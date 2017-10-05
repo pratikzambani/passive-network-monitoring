f3b3e211200348e535f071cde3ea214ccbc96beb
Pratik Zambani <pzambani@cs.stonybrook.edu>
2017-10-05 00:52:01 +0000

    ip packets

--- mydump.c
+++ mydump.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<signal.h>
+#include<time.h>
+#include<netinet/if_ether.h>
 #include<pcap.h>
 #include<arpa/inet.h>
 
@@ -64,6 +66,22 @@ struct sniff_tcp {
         u_short th_urp;                 /* urgent pointer */
 };
 
+/* UDP header */
+struct sniff_udp {
+        u_short uh_sport;
+        u_short uh_dport;
+        u_short uh_ulen;
+        u_short uh_sum;
+};
+
+/*
+struct timeval TimeStamp()
+{
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv;
+}*/
+
 volatile int run=1;
 
 void sigint_handler(int signum)
@@ -76,27 +94,86 @@ void pkt_receive_callback(u_char *args, const struct pcap_pkthdr *header, const 
   const struct sniff_ethernet *ethernet;
   const struct sniff_ip *ip;
   const struct sniff_tcp *tcp;
+  const struct sniff_udp *udp;
   const char *payload;
-
-  ethernet = (struct sniff_ethernet*)packet;
   
-  switch(ip->ip_p)
+  int size_ip;
+  int size_tcp;
+  int size_udp;
+  int size_payload;
+  //int caplen = header.caplen;
+
+  char fmt[64],buf[64];
+  struct timeval tv;
+  struct tm *tm;
+
+  gettimeofday(&tv, NULL);
+  tm = localtime(&tv.tv_sec);
+
+  if(tm != NULL)
   {
-    case IPPROTO_TCP:
-      printf("TCP");
-      break;
-    case IPPROTO_UDP:
-      printf("UDP");
-      break; 
-    case IPPROTO_ICMP:
-      printf("ICMP");
-      break;
-    case IPPROTO_IP:
-      printf("IP");
-      break;
-    default:
-      printf("Unknown protocol");
+    strftime(fmt, sizeof fmt, "%Y-%m-%d %H:%M:%S.%%06u ", tm);
+    snprintf(buf, sizeof buf, fmt, tv.tv_usec);
+    printf("%s",buf);
+  }
+   
+  ethernet = (struct sniff_ethernet*)packet;
+
+  printf("%02X:%02X:%02X:%02X:%02X:%02X",ethernet->ether_shost[0], ethernet->ether_shost[1], ethernet->ether_shost[2], ethernet->ether_shost[3], ethernet->ether_shost[4], ethernet->ether_shost[5]); 
+  
+  printf(" -> ");
+
+  printf("%02X:%02X:%02X:%02X:%02X:%02X",ethernet->ether_dhost[0], ethernet->ether_dhost[1], ethernet->ether_dhost[2], ethernet->ether_dhost[3], ethernet->ether_dhost[4], ethernet->ether_dhost[5]); 
+  
+  printf(" type %u 0x%04X ", (unsigned short)ethernet->ether_type, ethernet->ether_type);
+ 
+  if(ntohs(ethernet->ether_type) == ETHERTYPE_IP)
+  {
+    printf("len ");
+    ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip)*4;
+    if(size_ip < 20)
+    {
+      printf("Invalid IP header length %u bytes\n", size_ip);
       return;
+    }
+   
+    if(ip->ip_p == IPPROTO_TCP)
+    {
+      tcp = (struct sniff_tcp*)(packet+SIZE_ETHERNET+size_ip);
+      size_tcp = TH_OFF(tcp)*4;
+      if(size_tcp < 20)
+      {
+        printf("Invalid TCP header length %u bytes\n", size_tcp);
+        return;
+      }
+      printf("%s:%d -> %s:%d TCP ",inet_ntoa(ip->ip_src), ntohs(tcp->th_sport), inet_ntoa(ip->ip_dst), ntohs(tcp->th_dport));
+    }
+    else if(ip->ip_p == IPPROTO_UDP)
+    {
+      // inst.eecs.berkeley.edu/~ee122/fa07/projects/p2files/packet_parser.c 
+      udp = (struct sniff_udp*)(packet+SIZE_ETHERNET+size_ip);
+      /*size_udp = caplen - SIZE_ETHERNET - size_ip;
+      if(size_udp < sizeof(struct sniff_udp))
+      {
+        printf("Invalid UDP header length %u bytes\n", size_udp);
+        return;
+      }*/
+      printf("%s:%d -> %s:%d UDP ",inet_ntoa(ip->ip_src), ntohs(udp->uh_sport), inet_ntoa(ip->ip_dst), ntohs(udp->uh_dport));
+    }
+    else if(ip->ip_p == IPPROTO_ICMP)
+    {
+      printf("%s:%d -> %s:%d ICMP ",inet_ntoa(ip->ip_src), ntohs(tcp->th_sport), inet_ntoa(ip->ip_dst), ntohs(tcp->th_dport));
+    }  
+    else if(ip->ip_p == IPPROTO_IP)
+    {
+      printf("IP\n");
+    }
+    else
+    {
+      printf("Unknown proto %u %c \n", ip->ip_p, ip->ip_p);
+    }
+
   }
 }
 
